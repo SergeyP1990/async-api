3f33df1f75ee375c1a1e2906eeb8067d31e795f4
Artem Osokin <aptem21@mail.ru>
2022-02-17 14:01:41 +0300

    Поправил docker-compose для тестов (#35)

--- tests/functional/src/film_test.py
+++ tests/functional/src/film_test.py
@@ -29,7 +29,7 @@ async def test_get_films_data_by_filter_comedy_and_sort_asc(make_get_request):
 
 
 @pytest.mark.asyncio
-async def test_get_film_data_by_id(make_get_request):
+async def test_get_film_data_by_id_1(make_get_request):
     response = await make_get_request('film/2a090dde-f688-46fe-a9f4-b781a985275e')
     assert response.status == 200
     assert len(response.body) == 8
@@ -55,7 +55,60 @@ async def test_get_film_data_by_id(make_get_request):
 
 
 @pytest.mark.asyncio
+async def test_get_film_data_by_id_2(make_get_request):
+    response = await make_get_request('film/fc23ea9c-e799-419a-9df0-fc9d9b941a12')
+    assert response.status == 200
+    assert len(response.body) == 8
+    assert response.body['uuid'] == 'fc23ea9c-e799-419a-9df0-fc9d9b941a12'
+    assert response.body['title'] == 'Star Troopers'
+    assert response.body['imdb_rating'] == 4.7
+    assert response.body['description'] == 'In the closing stages of the' \
+                                           ' 21st Century, an enigmatic' \
+                                           ' warrior, called the Baron,' \
+                                           ' leads the crusade that the' \
+                                           ' religious organization Exorcio' \
+                                           ' Deus Machine is conducting' \
+                                           ' against the forces of Evil.' \
+                                           ' During an extermination mission,' \
+                                           ' the Baron is taken prisoner' \
+                                           ' by the evil sorceress Lady' \
+                                           ' Pervertvm and is tortured' \
+                                           ' until she gets his seed.' \
+                                           ' Lady Pervertvm uses the' \
+                                           ' Baron\'s seminal fluid to' \
+                                           ' fertilize the Beast-Ragnarok' \
+                                           ' in order to have it engender' \
+                                           ' the most powerful and ultimate' \
+                                           ' race of demons. The Baron manages' \
+                                           ' to escape but, blinded by his' \
+                                           ' guilt, he begins a bloody Via ' \
+                                           'Crucis which will start to reveal' \
+                                           ' his true nature.'
+    assert len(response.body['genre']) == 3
+    assert response.body['genre'][0] == {
+        "name": "Action",
+        "uuid": "3d8d9bf5-0d90-4353-88ba-4ccc5d2c07ff"
+    }
+    assert response.body['genre'][1] == {
+        "name": "Adventure",
+        "uuid": "120a21cf-9097-479e-904a-13dd7198c1dd"
+    }
+    assert response.body['genre'][2] == {
+        "name": "Fantasy",
+        "uuid": "b92ef010-5e4c-4fd0-99d6-41b6456272cd"
+    }
+    assert len(response.body['actors']) == 4
+    assert len(response.body['writers']) == 1
+    assert response.body['writers'][0] == {
+        "uuid": "9672c2ca-c113-4b3c-8a34-31edf0398a88",
+        "full_name": "Ricardo Ribelles"
+    }
+    assert response.body['directors'][0]['full_name'] == "Ricardo Ribelles"
+
+
+@pytest.mark.asyncio
 async def test_get_film_data_by_unknown_id(make_get_request):
     response = await make_get_request('film/ead9b449-734b-4878-86f1-1e4c96a28bba')
     assert response.status == 404
+    assert len(response.body) == 1
     assert response.body['detail'] == 'Film not found'
---
+++ tests/functional/src/genre_test.py
@@ -0,0 +1,34 @@
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_get_genre_data_default(make_get_request):
+    response = await make_get_request('genre/')
+    assert response.status == 200
+    assert len(response.body) == 50
+    assert response.body[0]['name'] == 'Action'
+    assert response.body[49]['name'] == 'News'
+
+
+@pytest.mark.asyncio
+async def test_get_genre_data_by_id_1(make_get_request):
+    response = await make_get_request('genre/f39d7b6d-aef2-40b1-aaf0-cf05e7048011')
+    assert response.status == 200
+    assert len(response.body) == 2
+    assert response.body['name'] == 'Horror'
+
+
+@pytest.mark.asyncio
+async def test_get_genre_data_by_id_2(make_get_request):
+    response = await make_get_request('genre/63c24835-34d3-4279-8d81-3c5f4ddb0cdc')
+    assert response.status == 200
+    assert len(response.body) == 2
+    assert response.body['name'] == 'Crime'
+
+@pytest.mark.asyncio
+async def test_get_genre_data_by_unknown_id(make_get_request):
+    response = await make_get_request('genre/f39d7b6d-aef2-40b1-aaf0-cf05432gv43g4')
+    assert response.status == 404
+    assert len(response.body) == 1
+    assert response.body['detail'] == 'Genre not found'
+
