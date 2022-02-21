e0233d70a4cb69d5e9522c0b773391aff44625a8
SergeyP1990 <totalmeltdown90@gmail.com>
2022-02-22 02:54:24 +0300

    Документация OpenAPI

--- src/main.py
+++ src/main.py
@@ -17,6 +17,8 @@ app = FastAPI(
     # Можно сразу сделать небольшую оптимизацию сервиса
     # и заменить стандартный JSON-сереализатор на более шуструю версию, написанную на Rust
     default_response_class=ORJSONResponse,
+    description="Информация о фильмах, жанрах и людях, участвовавших в создании произведения",
+    version="1.0.0"
 )
 
 
@@ -38,6 +40,6 @@ async def shutdown():
 
 # Подключаем роутер к серверу, указав префикс /v1/film
 # Теги указываем для удобства навигации по документации
-app.include_router(film.router, prefix='/api/v1/film', tags=['film'])
-app.include_router(genre.router, prefix='/api/v1/genre', tags=['genre'])
-app.include_router(person.router, prefix='/api/v1/person', tags=['person'])
+app.include_router(film.router, prefix='/api/v1/film')
+app.include_router(genre.router, prefix='/api/v1/genre')
+app.include_router(person.router, prefix='/api/v1/person')
