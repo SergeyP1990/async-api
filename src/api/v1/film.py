d600802436eb5da004eda19a10ce75ce659841c9
Sergey Plakunov <77925490+SergeyP1990@users.noreply.github.com>
2022-02-22 03:37:35 +0300

    Документация OpenAPI (#49)

--- src/api/v1/film.py
+++ src/api/v1/film.py
@@ -11,7 +11,21 @@ router = APIRouter()
 
 
 # Внедряем FilmService с помощью Depends(get_film_service)
-@router.get('/{film_id}', response_model=Film)
+@router.get('/{film_id}',
+            response_model=Film,
+            summary="Фильм по ID",
+            description="Вывод фильма:"
+                        "ID"
+                        "::Название фильма"
+                        "::Рейтинг IMDB"
+                        "::Описание"
+                        "::Жанры фильма"
+                        "::Список актёров"
+                        "::Список сценаристов"
+                        "::Режисёры",
+            response_description="Полная информация о фильме",
+            tags=['Информация по ID']
+            )
 async def film_details(film_id: str,
                        film_service: FilmService = Depends(get_film_service)
                        ) -> Film:
@@ -20,7 +34,10 @@ async def film_details(film_id: str,
         # Если фильм не найден, отдаём 404 статус
         # Желательно пользоваться уже определёнными HTTP-статусами, которые содержат enum
         # Такой код будет более поддерживаемым
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail=APIErrors.FILM_NOT_FOUND)
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail=APIErrors.FILM_NOT_FOUND
+        )
 
     # Перекладываем данные из models.Film в Film
     # Обратите внимание, что у модели бизнес-логики есть поле description
@@ -38,7 +55,14 @@ async def film_details(film_id: str,
                 directors=film.directors)
 
 
-@router.get('/')
+@router.get('/',
+            response_model=List[FilmSmall],
+            summary="Список кинопроизведений",
+            description="Список всех произведений "
+                        "(Пагинация по умолчанию - 50 элементов)",
+            response_description="Название и рейтинг фильма",
+            tags=['Список кинопроизведений']
+            )
 async def film_main(
         sort: str = "-imdb_rating",
         page_size: int = Query(50, alias="page[size]"),
@@ -60,7 +84,13 @@ async def film_main(
     return films
 
 
-@router.get('/search/')
+@router.get('/search/',
+            response_model=List[FilmSmall],
+            summary="Поиск кинопроизведений",
+            description="Полнотекстовый поиск по кинопроизведениям",
+            response_description="Название и рейтинг фильма",
+            tags=['Полнотекстовый поиск']
+            )
 async def film_search(
         query: str,
         page_size: int = Query(50, alias="page[size]"),
--- src/api/v1/person.py
+++ src/api/v1/person.py
@@ -10,30 +10,61 @@ from api.v1.error_messages import APIErrors
 router = APIRouter()
 
 
-@router.get("/{person_id}", response_model=Person)
+@router.get("/{person_id}",
+            response_model=Person,
+            summary="Персонаж по ID",
+            description="Информация о персонаже:"
+                        "ID"
+                        "::Полное имя"
+                        "::Список ролей в различных кинопроизведениях"
+                        "::Список ID фильмов с участием персонажа",
+            response_description="Полная информация о персонаже",
+            tags=['Информация по ID'])
 async def person_details(person_id: str,
                          person_service: PersonService = Depends(get_person_service)
                          ) -> Person:
     person = await person_service.get_person_by_id(person_id)
     if not person:
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail=APIErrors.PERSON_NOT_FOUND)
-
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail=APIErrors.PERSON_NOT_FOUND
+        )
     return person
 
 
-@router.get('/search/')
+@router.get('/search/',
+            response_model=List[Person],
+            summary="Поиск персонажей",
+            description="Полнотекстовый поиск по персонажам"
+                        " (Пагинация по умолчанию - 50 элементов)",
+            response_description="Информация о персонажах",
+            tags=['Полнотекстовый поиск']
+            )
 async def person_search(query: str,
                         page_size: int = Query(50, alias="page[size]"),
                         page_number: int = Query(1, alias="page[number]"),
                         person_service: PersonService = Depends(get_person_service)) -> List[Person]:
-    persons = await person_service.get_person_search(query, page_size, page_number)
+    persons = await person_service.get_person_search(
+        query,
+        page_size,
+        page_number
+    )
     return persons
 
 
-@router.get('/{person_id}/film')
+@router.get('/{person_id}/film',
+            response_model=List[FilmSmall],
+            summary="Список кинопроизведений по ID персонажа",
+            description="Список кинопроизведений определённого персонажа",
+            response_description="Полный список фильмов персонажа",
+            tags=['Информация по ID']
+            )
 async def person_search(person_id: str,
                         persons_service: PersonService = Depends(get_person_service)) -> List[FilmSmall]:
     data = await persons_service.get_films_by_person(person_id)
     if not data:
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail=APIErrors.PERSON_NOT_FOUND)
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail=APIErrors.PERSON_NOT_FOUND
+        )
     return data
