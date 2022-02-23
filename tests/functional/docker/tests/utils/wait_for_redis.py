2522b50c2b6a466ab2952be5acaa34768359e52f
SergeyP1990 <totalmeltdown90@gmail.com>
2022-02-24 01:41:41 +0300

    Изменение структуры и логики запуска тестов
    - entrypoint переносится в Dockerfile
    - waiters модифицируются в функции
    - sys.path.append заменяется на запуск main.py, в котором вызываются функции wait

--- tests/functional/docker/tests/utils/wait_for_redis.py
+++ tests/functional/docker/tests/utils/wait_for_redis.py
@@ -1,9 +1,7 @@
-import sys
 import time
 
 from redis import Redis, ConnectionError
 
-sys.path.append('/usr/src/tests/')
 from settings import test_settings
 
 r = Redis(
@@ -11,12 +9,14 @@ r = Redis(
     port=test_settings.redis_port
 )
 
-is_connected = False
 
-while not is_connected:
-    try:
-        is_connected = r.ping()
-        print('Redis connected.')
-    except ConnectionError:
-        print('Redis not connected. Wait 10 seconds...')
-        time.sleep(10)
+def wait_for_redis():
+    is_connected = False
+
+    while not is_connected:
+        try:
+            is_connected = r.ping()
+            print('Redis connected.')
+        except ConnectionError:
+            print('Redis not connected. Wait 10 seconds...')
+            time.sleep(10)
